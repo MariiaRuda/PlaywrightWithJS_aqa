e9140736e088fadc023944626f9b1d3dd85b7a8b
Mariia Ruda <masharuda290@gmail.com>
2026-06-24 12:23:34 +0300

    locators_css_basics

--- tests/UIBasicsTest.spec.js
+++ tests/UIBasicsTest.spec.js
@@ -14,10 +14,34 @@ test('Browser Playwright Test', async ({browser})=>
 
 //browser global variable tells Playwright to open new window with all existing browser data (cookies etc.)
 
-test.only('Page Playwright Test', async ({page})=>
+test('Page Playwright Test', async ({page})=>
 { 
     //now to direct url to go +await
     await page.goto("https://google.com");
     console.log(await page.title());
     await expect(page).toHaveTitle("Google");
 });
+
+test.only('Browser Context Playwright test', async ({browser})=>
+    {   //initialize browser window, tab and url
+        const context = await browser.newContext();
+        const page = await context.newPage();
+        await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+
+        //wait untill page of url is loaded - title appears and we catch it
+        console.log(await page.title());
+
+        //css locators to select any  element 
+        //enter username
+        await page.locator('#username').fill('rahulshetty');
+
+        //enter password
+        await page.locator('#password').fill('learning');
+
+        //click submit button
+        await page.locator('#signInBtn').click();
+
+        // wait for validation banner to show => console.log error message text of banner
+        console.log(await page.locator("[style*='block']").textContent());
+
+    });
